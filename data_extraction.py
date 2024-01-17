3c6b8250d01bede9983bb373169bb0d4cae00cfa
Samit-Ahmed <i.shaan03@hotmail.com>
2024-01-17 22:06:02 +0000

    Updated data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -11,20 +11,26 @@ class DataExtractor:
         self.rds_database = self.db.init_db_engine()
         self.api_key = {'x-api-key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
 
+    # extract the database table to a pandas DataFrame
     def read_rds_table(self, table_name):
         return pd.read_sql_table(table_name, self.rds_database)
 
+    # takes in a link as an argument and returns a pandas DataFrame.
     def retrieve_pdf_data(self, pdf_link):
         pdf_dataframe = tabula.read_pdf(pdf_link, pages="all", multiple_tables=True)
 
         return pd.concat(pdf_dataframe, ignore_index=True)
 
+    # returns the number of stores to extract.
+    # It should take in the number of stores endpoint and header dictionary as an argument.
     def list_number_of_store(self):
         stores = requests.get('https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores',
                               headers=self.api_key)
         number_of_stores = stores.json()
         return number_of_stores["number_stores"]
 
+    # will take the retrieve a store endpoint as an argument
+    # extracts all the stores from the API saving them in a pandas DataFrame.
     def retrieve_stores_data(self):
         number_of_stores = self.list_number_of_store()
         stores_list = []
@@ -34,6 +40,7 @@ class DataExtractor:
                 headers=self.api_key).json())
         return pd.json_normalize(stores_list)
 
+    # method will take the s3 address as an argument and return the pandas dataFrame
     def extract_from_s3(self):
         s3 = boto3.client('s3')
         bucket = 'data-handling-public'
@@ -43,8 +50,10 @@ class DataExtractor:
         table = pd.read_csv('./products.csv')
         return table
 
+    # The file is currently stored on S3 and can be found at the following link
+    # https://data-handling-public.s3.eu-west-1.amazonaws.com/date_details.json
     def extract_from_s3_json(self):
-        #https://data-handling-public.s3.eu-west-1.amazonaws.com/date_details.json
+
         s3 = boto3.client('s3')
         bucket = 'data-handling-public'
         object = 'date_details.json'
